aada39b3c000ef3f29ee068047994d5414e14631
Monica Hanson <mhanson@monicarose.tech>
2024-02-07 20:17:26 -0600

    Bad module checks
    Add support for required modules
    Add module error to output if relevant

--- main.py
+++ main.py
@@ -11,19 +11,89 @@ from modules import discord, homeassistant, mail
 
 CWD = os.path.dirname(os.path.realpath(__file__)) + "/"
 
+def friendly_size(size):
+    units = {
+        '0': 'B',
+        '1': 'KB',
+        '2': 'MB',
+        '3': 'GB',
+        '4': 'TB',
+        '5': 'PB'
+    }
+
+    unit = 0
+
+    while size > 1024 and unit < 5:
+        unit += 1
+        size = size / 1024
+
+    return f"{round(size, 2)} {units[f'{unit}']}"
+
+def calculate_size():
+
+    # Build remote information
+    with open(CWD + "options.json") as file:
+        options = json.load(file)
+        source = ""
+        dest = ""
+        if "source_remote" in options:
+            source += options["source_remote"] + ":"
+        if "source_path" in options:
+            source += options["source_path"]
+        if "remote" in options:
+            dest += options["remote"] + ":"
+        if "remote_path" in options:
+            dest += options["remote_path"]
+
+    # Run the check and format the diff
+    command = f"rclone check {source} {dest} --combined -"
+    process = subprocess.run(command.split(" "), capture_output=True)
+    diffs = process.stdout.decode().strip().split("\n")
+
+    files_to_backup = []
+    for diff in diffs:
+        diff = diff.split(" ")
+        # Ignore matches or files only the destination has
+        if diff[0] == '-' or diff[0] == '=':
+            pass
+        else:
+            file = " ".join(diff[1:])
+            file_stats = os.stat("D:/" + file)
+            files_to_backup.append([file, file_stats.st_size])
+
+    return files_to_backup
+
 def backup(modules):
     remote = 'encrypted'
 
-    # Backup information
-    data = {
-        "source": socket.gethostname(),
-        "remote": remote,
-        "status": "starting"
-    }
+    diff = calculate_size()
+    if len(diff) == 0:
+        data = {
+            "source": socket.gethostname(),
+            "remote": remote,
+            "status": "cancel"
+        }
+    else:
+        # Backup information
+        data = {
+            "source": socket.gethostname(),
+            "remote": remote,
+            "status": "starting",
+            "count": len(diff),
+            "size": sum(size[1] for size in diff)
+        }
 
     # Notify each of the specified modules
+    failed_modules = []
     for module in modules:
-        module.send(data)
+        try:
+            module.send(data)
+        except Exception as e:
+            print(f"Warning: module {module.name} failed when pushing alert")
+            failed_modules.append({
+                "module": module.name,
+                "exception": e.__class__.__name__
+            })
 
     start_time = datetime.now()
 
@@ -91,8 +161,18 @@ def backup(modules):
         "remote": remote,
         "status": "complete",
         "result": process.returncode,
-        "time": total_time
+        "time": total_time,
+        "count": len(diff),
+        "size": sum(size[1] for size in diff)
     }
+
+    # If we have any failed modules add them to the report
+    if len(failed_modules) > 0:
+        data["failed_modules"] = []
+        for failed_module in failed_modules:
+            data["failed_modules"].append({
+                "name": failed_module.name
+            })
 
     for module in modules:
         module.send(data)
@@ -109,23 +189,34 @@ def main(args):
     modules = []
 
     if "discord" in secrets:
-        webhook = secrets["discord"]["webhook"]
-        modules.append(discord.Discord(webhook))
+        discord_secrets = secrets["discord"]
+        required = discord_secrets["required"] if discord_secrets["required"] else False
+        modules.append(discord.Discord(discord_secrets["webhook"], ))
     if "homeassistant" in secrets:
-        server = secrets["homeassistant"]["server"]
-        api_key = secrets["homeassistant"]["api_key"]
-        modules.append(homeassistant.HomeAssistant(api_key, server))
+        homeassistant_secrets = secrets["homeassistant"]
+        server = homeassistant_secrets["server"]
+        api_key = homeassistant_secrets["api_key"]
+        required = homeassistant_secrets["required"] if homeassistant_secrets["required"] else False
+        modules.append(homeassistant.HomeAssistant(api_key, server, required))
     if "mail" in secrets:
-        server = secrets["mail"]["server"]
-        port = secrets["mail"]["port"]
-        sender = secrets["mail"]["sender"]
-        recipients = secrets["mail"]["recipients"]
-        username = secrets["mail"]["username"] if secrets["mail"]["username"] else sender
-        password = secrets["mail"]["password"]
-        protocol = secrets["mail"]["protocol"]
-        modules.append(mail.Mail(recipients, sender, username, password, server, port, protocol))
+        mail_secrets = secrets["mail"]
+        server = mail_secrets["server"]
+        port = mail_secrets["port"]
+        sender = mail_secrets["sender"]
+        recipients = mail_secrets["recipients"]
+        username = mail_secrets["username"] if mail_secrets["username"] else sender
+        password = mail_secrets["password"]
+        protocol = mail_secrets["protocol"]
+        required = mail_secrets["required"] if mail_secrets["required"] else False
+        modules.append(mail.Mail(recipients, sender, username, password, server, port, protocol, required))
 
-    backup(modules)
+    diff = calculate_size()
+    for file in diff:
+        print(file)
+    print(f"Number of files to back up: {len(diff)}")
+    print(f"Size of files to back up: {friendly_size(sum(size[1] for size in diff))}")
+
+    # backup(modules)
 
 if __name__ == "__main__":
     main(sys.argv)
